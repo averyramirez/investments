25754c3641a3de5532c11e9bb6cfd01c282f584e
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2024-01-05 11:45:29 -0800

    Investments
    Used the chart.js api to create a line graph using data populated from a json file to display the monthly value of investments over time

---
+++ app.js
@@ -0,0 +1,64 @@
+const ctx = document.getElementById("myChart");
+
+const priceData = await fetchPriceData();
+console.log("This is the priceData:", priceData);
+
+const dateData = await fetchDateData();
+console.log("This is the dateData:", dateData);
+
+new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: dateData,
+    datasets: [
+      {
+        label: "Dollar Value",
+        data: priceData,
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+async function fetchPriceData() {
+  //Fetch json data
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  //Declare price array
+  const priceArr = [];
+
+  //Iterate over data object
+  data.forEach((obj) => {
+    //Update price array
+    let price = Math.round(obj.PRICE);
+    priceArr.push(price);
+  });
+  console.log("Price array:", priceArr);
+  return priceArr;
+}
+
+async function fetchDateData() {
+  //Fetch json data
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  //Declare date array
+  const dateArr = [];
+
+  //Iterate over data object
+  data.forEach((obj) => {
+    //Update date array
+    let date = obj.DATE;
+    dateArr.push(date);
+  });
+  console.log("Date array:", dateArr);
+  return dateArr;
+}
